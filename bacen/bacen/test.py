21f14e159f682cfecf977d678ea29e86a346c4a2
Bernardo Paulsen <bernardohpaulsen@gmail.com>
2021-07-04 17:45:12 -0300

    test.pypi

--- bacen/bacen/test.py
+++
@@ -1,5 +0,0 @@
-import bacen
-
-x = bacen.get(12,"01/01/2000", "01/01/2020")
-
-print(x)
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pandas>=1.2"]
+
+setup(
+    name="bacen",
+    version="0.9.1",
+    author="Bernardo Paulsen",
+    author_email="bernardopaulsen@gmail.com",
+    description="A package to import data frmo BACEN's Time Series Management System",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bernardopaulsen/bacen",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
